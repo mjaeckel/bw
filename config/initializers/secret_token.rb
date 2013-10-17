2f72d04be20a01367c197e0e47ad515fb4438507
Marlene Jaeckel <marlene.jaeckel@gmail.com>
2013-10-17 17:39:34 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kinetix::Application.config.secret_key_base = '00e6a8898c0fe5901709416f6840eed611792a20df1172b178199f80b96fdd944b6dcc7fce96d86f12a64706eb8186855bd669e682bd03cf353b87b2a75bf3c1'
